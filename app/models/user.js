c2a37e4129682e87dd9c4ff8083182e2594568b2
Jerson López Castaño <jersonlopez.1204@gmail.com>
2018-09-28 10:57:20 -0500

    user model and migration done (#3)
    * user model and migration done
    
    * fix PR #3 comments
    
    * Solving 'Last little changes' from PR #3

---
+++ app/models/user.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'users',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      paranoid: true,
+      freezeTableName: true,
+      underscored: true
+    }
+  );
+
+  return User;
+};
---
+++ migrations/migrations/20180925215318-user-migration.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: Sequelize.DATE,
+      updated_at: Sequelize.DATE,
+      deleted_at: Sequelize.DATE
+    }),
+
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('users')
+};
