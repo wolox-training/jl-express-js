89ade93abaa23ad1bd4d64b5ba70dab57d8db2d0
jersonlopez <jersonlopez.1204@gmail.com>
2018-09-27 19:32:35 -0500

    signup user done

---
+++ migrations/migrations/20180927200242-user-migration.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: Sequelize.DATE,
+      updated_at: Sequelize.DATE,
+      deleted_at: Sequelize.DATE
+    }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('users')
+};
