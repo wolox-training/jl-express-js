186ac9dc0f35bb27d256530a5f5dcd2adfe260dd
jersonlopez <jersonlopez.1204@gmail.com>
2018-10-10 09:49:59 -0500

    change bcrypt by bcryptjs

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const bcrypt = require('bcrypt'),
+const bcrypt = require('bcryptjs'),
   saltRounds = 10,
   salt = bcrypt.genSaltSync(saltRounds),
   errors = require('../errors'),
